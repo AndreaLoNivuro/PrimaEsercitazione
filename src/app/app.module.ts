d735a55fe66c68a920d0e0be58f9530ff54da04e
AndreaLoNivuro <a.lonivuro@live.it>
2020-03-20 00:25:40 +0100

    Creati componenti e pagina app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { HomeComponent } from './home/home.component';
+import { GamesListComponent } from './games-list/games-list.component';
+import { GameDetailComponent } from './game-detail/game-detail.component';
+import { EditGameComponent } from './edit-game/edit-game.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    HomeComponent,
+    GamesListComponent,
+    GameDetailComponent,
+    EditGameComponent
   ],
   imports: [
     BrowserModule
